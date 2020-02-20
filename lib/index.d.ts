46527a361ae35f55f74ddc15d87088c08b5d16ab
Jianbing (Bruce) Li <Bruce.Li@navico.com>
2020-02-21 11:13:40 +1300

    Add lib in git

---
+++ lib/index.d.ts
@@ -0,0 +1,31 @@
+import { EmitterSubscription } from 'react-native';
+export declare const enum FtpFileType {
+    Dir = "dir",
+    File = "file",
+    Link = "link",
+    Unknown = "unknown"
+}
+export interface ListItem {
+    name: string;
+    type: FtpFileType;
+    size: number;
+    timestamp: Date;
+}
+export interface FtpSetupConfiguration {
+    ip_address: string;
+    port: number;
+    username: string;
+    password: string;
+}
+declare module FtpClient {
+    function setup(config: FtpSetupConfiguration): void;
+    function list(remote_path: string): Promise<Array<ListItem>>;
+    function uploadFile(local_path: string, remote_path: string): Promise<void>;
+    function cancelUploadFile(token: string): Promise<void>;
+    function addProgressListener(listener: (data: {
+        token: string;
+        percentage: number;
+    }) => void): EmitterSubscription;
+    function remove(remote_path: string): Promise<void>;
+}
+export default FtpClient;
